3a072a75f82773f92a0d3a2facd4e7f9e52e39b8
Maleena <maleena.sohail@gmail.com>
2023-01-19 13:57:17 +0530

    app tested

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const app = express()
+require('./models/order')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+mongoose.connect("mongodb://localhost:27017/delivery-service",
+    () => { console.log("Connected to Mongoose") })
+require("./routes/order.routes")(app)
+app.listen(7500, () => {
+    console.log("Server now on port 7500")
+})
